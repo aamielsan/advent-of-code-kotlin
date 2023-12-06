bfcb81e2ada5fab6a425408602e3a37a2138a74f
Adrian Sanchez <aamiel.sanchez@gmail.com>
2023-12-06 17:28:35 +0100

    Update day 6 solution to read input and enable tests

--- 2023/src/Day06.kt
+++ 2023/src/Day06.kt
@@ -1,38 +1,36 @@
 import kotlin.math.abs
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main() {
     // Seems to be a basic non-linear problem:
     // x^2 - time * x + distance <= 0 ; where x is the hold time
-    val test: String = """
-        Time:      7  15
-        Distance:  9  40
-    """.trimIndent()
+    val test = """
+        Time:      7  15   30
+        Distance:  9  40  200
+    """.trimIndent().lines()
 
-    fun numberOfWaysToWin(time: Double, distance: Double): Double {
-        val quadratic = sqrt(time.pow(2.0) - (4 * distance))
-        val firstRoot = ceil((time + quadratic) / 2)
-        val secondRoot = ceil((time - quadratic) / 2)
-
-        return abs(firstRoot - secondRoot)
-    }
-
-    fun part1(input: String): Double =
+    fun part1(input: List<String>) =
         input
-            .split("\n")
             .map { it.split(":").last().split(" ").filter(String::isNotEmpty).map(String::toDouble) }
             .let { (time, distance) -> time.zip(distance) }
-            .fold(1.0) { acc, (time, distance) -> acc * numberOfWaysToWin(time, distance) }
+            .fold(1) { acc, (time, distance) -> acc * numberOfWaysToWin(time, distance) }
 
-    fun part2(input: String): Double = numberOfWaysToWin(47847467.0, 207139412091014.0)
+    fun part2(input: List<String>) =
+        input
+            .map { it.split(":").last().replace(" ", "").toDouble() }
+            .let { (time, distance) -> numberOfWaysToWin(time, distance) }
 
-    val input: String = inputAsText("Day06")
+    val input: List<String> = inputAsLines("Day06")
 
-//    check(part1(test).println() == 32.0)
+    check(part1(test).println() == 288)
     part1(input).println()
-//    check(part2(test).println() == 47.toLong())
-    part2(input).toBigDecimal().toPlainString().println()
+    check(part2(test).println() == 71503)
+    part2(input).println()
+}
+
+private fun numberOfWaysToWin(time: Double, distance: Double): Int {
+    val quadratic = sqrt((time * time) - (4 * distance + 1))
+    val firstRoot = ((time + quadratic) / 2)
+    val secondRoot = ((time - quadratic) / 2)
+    return abs(firstRoot.toInt() - secondRoot.toInt())
 }
