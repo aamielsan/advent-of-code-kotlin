aa5878c2cab8452fb768837649a7fed9d300b453
Adrian Sanchez <aamiel.sanchez@gmail.com>
2023-12-09 09:14:16 +0100

    Add day 9 solution

--- 2023/src/Day09.kt
+++ 2023/src/Day09.kt
@@ -1,19 +1,56 @@
 fun main() {
     val test = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
     """.trimIndent().lines()
 
     fun part1(input: List<String>): Int {
-        return input.size
+        fun predictionFrom(history: List<Int>): Int {
+            val pairs = history.zipWithNext()
+            return when {
+                pairs.all { it.second - it.first == 0 } -> history.first()
+                else -> {
+                    val prediction = predictionFrom(pairs.map { it.second - it.first })
+                    prediction + history.last()
+                }
+            }
+        }
+
+        return input
+            .sumOf { line ->
+                line
+                    .split(" ")
+                    .map(String::toInt)
+                    .let(::predictionFrom)
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun predictionFrom(history: List<Int>): Int {
+            val pairs = history.zipWithNext()
+            return when {
+                pairs.all { it.second - it.first == 0 } -> history.first()
+                else -> {
+                    val prediction = predictionFrom(pairs.map { it.second - it.first })
+                    history.first() - prediction
+                }
+            }
+        }
+
+        return input
+            .sumOf { line ->
+                line
+                    .split(" ")
+                    .map(String::toInt)
+                    .let(::predictionFrom)
+            }
     }
 
-    val input: List<String> = inputAsLines("Day06")
+    val input: List<String> = inputAsLines("Day09")
 
-    check(part1(test).println() == 1)
+    check(part1(test) == 114)
     part1(input).println()
-    check(part2(test).println() == 1)
+    check(part2(test) == 2)
     part2(input).println()
 }
